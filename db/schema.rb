9bb74236dc8c8d06f113ed7be23617360f6a8e17
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-01-17 12:50:54 +0100

    Slightly better db:seed, references #12.

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(:version => 20110513191556) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-User.create!(:first_name => 'Dart', :last_name => 'Vader', :email => 'vader@deathstar.net')
-User.create!(:first_name => 'Luke', :last_name => 'Skywalker', :email => 'luke@rebel.org')
+User.create!(:first_name => "Dart", :last_name => "Vader", :email => "vader@deathstar.net")
+User.create!(:first_name => "Luke", :last_name => "Skywalker", :email => "luke@rebel.org")
+User.create!(:first_name => "William", :last_name => "Adama", :email => "adama@galactica.battlestar.org")
+
+50.times do |n|
+  User.create!(:first_name => "User #{n}", :last_name => "Last", :email => "user_#{n}@email.com")
+end
