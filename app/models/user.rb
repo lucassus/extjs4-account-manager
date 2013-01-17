1414288ffa4e7f9c9aa55c581b4c6ff1da891db5
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-01-17 16:30:13 +0100

    Validate email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, email: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,7 @@ describe User do
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+
+  it { should allow_value("user@email.com").for(:email) }
+  it { should_not allow_value("invalid@email").for(:email) }
 end
